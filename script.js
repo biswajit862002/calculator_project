3a22b6dc7049af96a0c44254ed11b6c487835d39
biswajit dey <112199301+biswajit862002@users.noreply.github.com>
2022-12-17 22:32:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+let outputscreen = document.getElementById("outputScreen"); 
+function display(num){
+    outputScreen.value += num;
+}
+
+function Calculate(){
+    try{
+        outputScreen.value = eval(outputScreen.value);
+    }
+
+    catch(err)
+    {
+        alert("Invalid Operation !!!");
+    }
+}
+
+    function Clear(){
+        outputScreen.value = "";
+    }
+
+    function del(){
+        outputScreen.value = outputScreen.value.slice(0,-1);
+    }
